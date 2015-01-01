2cba676f585a7e882a168af4b5120248ce1043f1
Jóannes í Sandagerði <eplamos.mos@gmail.com>
2015-01-01 17:32:07 +0100

    Cleanup.

--- src/main/java/com/sandagerdi/translationprojectmanager/TableModels/ClientTableModel.java
+++ src/main/java/com/sandagerdi/translationprojectmanager/TableModels/ClientTableModel.java
@@ -9,7 +9,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.table.AbstractTableModel;
